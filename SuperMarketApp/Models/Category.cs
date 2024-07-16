9c70425e3e6c7bb833542b3509854764a820b705
akpinki <akpinki@AJEETMISHRA>
2024-07-16 13:10:20 +0530

    Add project files.

---
+++ SuperMarketApp/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperMarketApp.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
