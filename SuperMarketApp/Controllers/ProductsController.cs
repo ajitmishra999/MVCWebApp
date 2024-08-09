12221249553dffa893af2918c546a2867a3c96f4
Ajeet Mishra <Ajeet.Mishra@russellreynolds.com>
2024-08-09 15:44:39 +0100

    commit after  product controller

---
+++ SuperMarketApp/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperMarketApp.Models;
+using SuperMarketApp.ViewModels;
+
+namespace SuperMarketApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = ProductsRepository.GetProducts(true);
+            return View(product);
+        }
+        public IActionResult Add()
+        {
+            var productViewModel = bindProductCategories();
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductsRepository.AddProduct(productViewModel.product);
+                return RedirectToAction(nameof(Index));
+            }
+            productViewModel = bindProductCategories();
+            return View(productViewModel);
+        }
+
+        private ProductViewModel bindProductCategories()
+        {
+            ProductViewModel productViewModel = new ProductViewModel()
+            {
+                categories = CategoriesRepository.GetCategories()
+            };
+
+            return productViewModel;
+        }
+    }
+}
---
+++ SuperMarketApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿
+using SuperMarketApp.Models;
+
+namespace SuperMarketApp.ViewModels
+{
+    public class ProductViewModel
+    {
+        public IEnumerable<Category> categories { get; set; } = Enumerable.Empty<Category>();
+        public Product product { get; set; } = new Product();
+    }
+}
